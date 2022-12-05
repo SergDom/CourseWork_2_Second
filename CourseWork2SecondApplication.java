82a59bdf2081232809942df0624eb3a2b920b025
sergeydomanov <nomad-labs@yandex.ru>
2022-12-05 12:03:18 +0300

    new commit

---
+++ CourseWork2SecondApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.coursework_2_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2SecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2SecondApplication.class, args);
+    }
+
+}
